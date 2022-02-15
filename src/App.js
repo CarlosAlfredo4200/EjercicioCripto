fa4f718f9bdb3e3329b1b559a6f9cbbf2c6a9836
Carlos Alfredo Montoya Goez <CarlosMontoya@comfama.com.co>
2022-02-15 16:11:33 -0500

    Cotizador Criptomonedas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import styled from "@emotion/styled";
+import Formulario from "./componentes/Formulario";
+import ImagenCripto from "./img/imagen-criptos.png";
+import Resultado from './componentes/Resultado';
+import Spinner from './componentes/Spinner';
 
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  
+   
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+  text-align:center;
+  font-weight:700;
+  margin-top: 60px;
+  margin-bottom:50px;
+  font-size:28px;
+
+  &::after{
+     content: '';
+     width: 90%;
+     height: 3px;
+     background-color: rgb(255,255,255, 0.3);
+     border-radius: 30px;
+     display: block;
+     margin: 10px auto 0 auto;
+ }
+`;
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 60px auto 0 auto;
+  display: block;
+`;
+
+
+ 
 function App() {
+
+  const [monedas, setMonedas] = useState({});
+  const [resultado, setResultado] = useState({});
+  const [cargango, setCargango] = useState(false)
+
+  
+
+
+
+ useEffect(() => {
+   if(Object.keys(monedas).length > 0){
+
+     const cotizarCripto = async () =>{
+       setCargango(true)
+       setResultado({})
+
+
+       
+       
+       
+       const {moneda, criptomoneda} = monedas;
+       const urlPrecios =`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+       
+       const respuesta = await fetch(urlPrecios)
+       const resultado = await respuesta.json()
+       setTimeout(() => {
+         setResultado(resultado.DISPLAY [criptomoneda][moneda])
+         setCargango(false)
+         
+         
+        }, 3000);
+     }
+
+     
+     cotizarCripto()
+   }
+ }, [monedas])
+ 
+
+  
+console.log(resultado);
+
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    
+    <Contenedor>
+      <Imagen src={ImagenCripto} alt="imagen"/>
+      <div>
+      <Heading>Cotiza Criptomonedas al instante</Heading>
+      <Formulario 
+      monedas={monedas}
+      setMonedas={setMonedas} 
+        
+      />
+       
+
+      {cargango && <Spinner />}
+      
+      { resultado.PRICE && <Resultado  resultado={resultado}/>}
+        
+        
+      </div>
+
+      
+      
+    </Contenedor>
+
   );
 }
 
---
+++ src/componentes/Data/Monedas.jsx
@@ -0,0 +1,11 @@
+  //Crear arreglo 
+  const monedas = [
+    {id:'USD', nombre:'Dolar estadounidense'},
+    {id:'MXM', nombre:'Peso Mexicano'},
+    {id:'EUR', nombre:'Euro'},
+    {id:'GBP', nombre:'Libra esterlina'}
+  ]
+
+  export {
+      monedas
+  }
